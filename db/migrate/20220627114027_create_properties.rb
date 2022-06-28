311b4d843346d8bb45a7e699ff74ffc0018b754c
Kei <kei.n140802@gmail.com>
2022-06-28 10:56:34 +0900

    model nearest_station を作成

--- db/migrate/20220627114027_create_properties.rb
+++ db/migrate/20220627114027_create_properties.rb
@@ -1,6 +1,11 @@
 class CreateProperties < ActiveRecord::Migration[6.0]
   def change
     create_table :properties do |t|
+      t.string :name
+      t.integer :price
+      t.string :address
+      t.integer :age
+      t.text :memo
 
       t.timestamps
     end
